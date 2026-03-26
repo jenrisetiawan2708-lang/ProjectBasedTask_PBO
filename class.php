094f3f00ddeb29babafc9fb6c24b32907f8cbc03
ardli <achululum09@gmail.com>
2026-03-26 16:16:08 +0700

    Update class biodata

--- class.php
+++ class.php
@@ -6,17 +6,21 @@ class Biodata {
     public $address;
 
     public function __construct($firstname, $lastname, $phone, $address) {
-        $this->firstname = htmlspecialchars($firstname);
-        $this->lastname = htmlspecialchars($lastname);
-        $this->phone = htmlspecialchars($phone);
-        $this->address = htmlspecialchars($address);
+        $this->firstname = htmlspecialchars(trim($firstname));
+        $this->lastname = htmlspecialchars(trim($lastname));
+        $this->phone = htmlspecialchars(trim($phone));
+        $this->address = htmlspecialchars(trim($address));
+    }
+
+    public function getFullName() {
+        return $this->firstname . " " . $this->lastname;
     }
 
     public function tampilkan() {
         return "
         <div class='result'>
-            <p><b>Nama:</b> {$this->firstname} {$this->lastname}</p>
-            <p><b>Phone:</b> {$this->phone}</p>
+            <p><b>Nama Lengkap:</b> " . $this->getFullName() . "</p>
+            <p><b>Phone Number:</b> {$this->phone}</p>
             <p><b>Address:</b> {$this->address}</p>
         </div>
         ";
